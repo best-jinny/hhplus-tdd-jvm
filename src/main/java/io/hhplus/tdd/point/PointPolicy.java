f88d7097ee3b34f448d506e818ffe37b4b547063
hyejin <iwilldotherightthings@gmail.com>
2024-12-18 21:14:39 +0900

    포인트 정책 설정

---
+++ src/main/java/io/hhplus/tdd/point/PointPolicy.java
@@ -0,0 +1,10 @@
+package io.hhplus.tdd.point;
+
+public class PointPolicy {
+    public static final long MIN_AMOUNT_PER_CHARGE = 1000;
+    public static final long MAX_TOTAL_AMOUNT = 1000000;
+    public static final long CHARGE_UNIT = 1000;
+    public static final long MAX_AMOUNT_PER_CHARGE = 100000;
+    public static final long USE_UNIT = 100;
+    public static final long MIN_AMOUNT_PER_USE = 100;
+}
